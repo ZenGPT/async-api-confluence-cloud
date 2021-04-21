f1ceb389d47807fc660c9c07628d67086904eb04
MrCoder <eagle.xiao@gmail.com>
2021-04-21 21:27:39 +1000

    Added react router.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
 
 import Playground from './Playground';
 
@@ -7,4 +12,22 @@ import '@fortawesome/fontawesome-svg-core/styles.css';
 import '@asyncapi/react-component/lib/styles/fiori.css';
 import './common/icons';
 
-ReactDOM.render(<Playground />, document.getElementById('root'));
+function App() {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/create">
+                    <Playground/>
+                </Route>
+                <Route path="/view">
+                    <div>View</div>
+                </Route>
+                <Route path="/">
+                    <div>Home</div>
+                </Route>
+            </Switch>
+        </Router>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));

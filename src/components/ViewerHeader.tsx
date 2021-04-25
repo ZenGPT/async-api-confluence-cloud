1813449832e398b295f85b7680e53f4621dcc084
MrCoder <eagle.xiao@gmail.com>
2021-04-25 19:41:30 +1000

    Open 'Edit' page when clicking 'Edit'.

--- src/components/ViewerHeader.tsx
+++ src/components/ViewerHeader.tsx
@@ -3,12 +3,21 @@ import Button from "@atlaskit/button";
 import EditIcon from "@atlaskit/icon/glyph/edit";
 
 export default function ViewerHeader() {
+  function editApiDoc() {
+    // @ts-ignore
+    AP.dialog.create({
+      key: 'editApiDoc',
+      chrome: false
+    });
+  }
+
   return (
     <>
       <div className="m-1 text-right">
         <Button
           iconBefore={<EditIcon label="Edit icon" size="small" />}
           appearance="primary"
+          onClick={editApiDoc}
         >
           Edit
         </Button>

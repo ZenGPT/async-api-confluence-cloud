064a3f3e09c797154bd1e34d91bfb2c3dd693658
MrCoder <eagle.xiao@gmail.com>
2021-04-30 08:38:30 +1000

    Fixed descriptor

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,17 @@
 import * as functions from "firebase-functions";
-import * as descriptorTemplate from "./atlassian-connect.json"
+import * as descriptorTemplate from "./atlassian-connect.json";
 
 export const descriptor = functions.https.onRequest((req, resp) => {
   const url = req.url;
-  const basePath = url.substring(0, url.lastIndexOf('/'));
-  const self = url.substring(url.lastIndexOf('/'));
+  const basePath = url.substring(0, url.lastIndexOf("/"));
+  const self = url.substring(url.lastIndexOf("/"));
   const result = Object.assign({},
-    descriptorTemplate,
-    {
-    baseUrl: `${req.protocol}://${req.hostname}${basePath}`,
-    links: {
-      self: self
-    }
-  });
+      descriptorTemplate,
+      {
+        baseUrl: `${req.protocol}://${req.hostname}${basePath}`,
+        links: {
+          self: self,
+        },
+      });
   resp.json(result);
 });

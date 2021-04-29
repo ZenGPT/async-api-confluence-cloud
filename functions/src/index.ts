e9bbc044b8da9d4b64a5ba1587366fa2c700bd01
MrCoder <eagle.xiao@gmail.com>
2021-04-29 18:35:20 +1000

    Added descriptor

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,17 @@
 import * as functions from "firebase-functions";
+import * as descriptorTemplate from "./atlassian-connect.json"
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+export const descriptor = functions.https.onRequest((req, resp) => {
+  const url = req.url;
+  const basePath = url.substring(0, url.lastIndexOf('/'));
+  const self = url.substring(url.lastIndexOf('/'));
+  const result = Object.assign({},
+    descriptorTemplate,
+    {
+    baseUrl: `${req.protocol}://${req.hostname}${basePath}`,
+    links: {
+      self: self
+    }
+  });
+  resp.json(result);
 });

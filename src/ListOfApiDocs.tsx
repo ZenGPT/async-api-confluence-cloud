c1afa2742c5d90076322da7cdcc3c342c61d5519
MrCoder <eagle.xiao@gmail.com>
2021-04-30 15:43:08 +1000

    Added log and changed the listing page title.

--- src/ListOfApiDocs.tsx
+++ src/ListOfApiDocs.tsx
@@ -70,7 +70,7 @@ export default function ListOfApiDocs(this: any) {
     <>
       <section className="px-4 sm:px-6 lg:px-4 xl:px-6 pt-4 pb-4 sm:pb-6 lg:pb-4 xl:pb-6 space-y-4">
         <header className="flex items-center justify-between">
-          <h2 className="text-lg leading-6 font-medium text-black">Projects</h2>
+          <h2 className="text-lg leading-6 font-medium text-black">Async API Documents</h2>
           <button
             className="hover:bg-light-blue-200 hover:text-light-blue-800 group flex items-center rounded-md bg-light-blue-100 text-light-blue-600 text-sm font-medium px-4 py-2">
             <svg className="group-hover:text-light-blue-600 text-light-blue-500 mr-2" width="12" height="20"

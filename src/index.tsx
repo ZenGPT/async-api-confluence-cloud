5f99b69ee4fcf5c5abe3a84cfcc22c0cbc12dbd4
MrCoder <eagle.xiao@gmail.com>
2021-05-18 21:15:05 +1000

    Replaced default path '/' with ListOfApiDocs.

--- src/index.tsx
+++ src/index.tsx
@@ -32,9 +32,7 @@ function App() {
                     <ListOfApiDocs/>
                 </Route>
                 <Route path="/">
-                  <input
-                    className="focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-10"
-                    type="text" aria-label="Filter projects" placeholder="Filter projects"/>
+                  <ListOfApiDocs/>
                 </Route>
             </Switch>
         </Router>

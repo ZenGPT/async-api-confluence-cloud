d566985c15aac05ab89647d2eabe55fba98121f3
Yanhui Li <whimet@gmail.com>
2024-01-11 22:14:48 +1100

    Send installed/uninstalled events to mixpanel

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,8 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.renderAttachment = exports.descriptor = void 0;
+exports.uninstalled = exports.installed = exports.renderAttachment = exports.descriptor = void 0;
 const functions = require("firebase-functions");
 const descriptorTemplate = require("./atlassian-connect.json");
+var Mixpanel = require('mixpanel');
+//Mixpanel project: Confluence Analytics
+var mixpanel = Mixpanel.init('0c62cea9ed2247f4824bf196f6817941');
 exports.descriptor = functions.https.onRequest((req, resp) => {
     const url = req.url;
     const basePath = url.substring(0, url.lastIndexOf("/"));
@@ -17,5 +20,57 @@ exports.descriptor = functions.https.onRequest((req, resp) => {
 });
 exports.renderAttachment = functions.https.onRequest((request, response) => {
     response.send(`<ac:image> <ri:attachment ri:filename="zenuml-${request.query.uuid}.png" /> </ac:image>`);
+});
+function handleLifecycleEvent(req) {
+    if (!req.body) {
+        return;
+    }
+    const clientSite = new URL(req.body.baseUrl).hostname;
+    mixpanel.track(req.body.eventType, {
+        distinct_id: clientSite,
+        clientSite,
+        key: req.body.key,
+        eventType: req.body.eventType,
+        serverVersion: req.body.serverVersion,
+        pluginVersion: req.body.pluginsVersion,
+        productType: req.body.productType,
+        description: req.body.description,
+        displayProductName: 'Async API'
+    });
+}
+exports.installed = functions.https.onRequest((req, resp) => {
+    /* example request.body:
+     {
+      key: 'my-api',
+      clientKey: 'aa4a743a-201f-38c4-b7fd-d7f8f68ec685',
+      publicKey: 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC+75i7nBJb4J46KQigYauY3FRM2QyMoVb/YmiygAnmH7aj4mz1iAVUVh/wRyvMtA/Aq1bROtM2Pfu23bM/Z4hDJtC+53OMf/DVTILkSpN8AF6QRO0DcvjPFHDeKhcgQgtYh4q/tjLu38nmKe6yjzILk5TxGlCc/V9w5NO5fHs0oQIDAQAB',
+      sharedSecret: 'ATCOfzFTe2Wc3Rh3FRV2h9CUlZCBRpUhIF5LXKDgv1Ru17afsZ4_fPQRqx1PZd21mBMIM0RDjU-uZPxMegaUfwRR_A0678FE89',
+      serverVersion: '6452',
+      pluginsVersion: '1000.0.0.57d9ee1272a4',
+      baseUrl: 'https://whimet4.atlassian.net/wiki',
+      productType: 'confluence',
+      description: 'Atlassian Confluence at null',
+      eventType: 'installed'
+    }
+    */
+    handleLifecycleEvent(req);
+    resp.send(`installed`);
+});
+exports.uninstalled = functions.https.onRequest((req, resp) => {
+    /** example request.body:
+    {
+      key: 'my-api',
+      clientKey: 'aa4a743a-201f-38c4-b7fd-d7f8f68ec685',
+      publicKey: 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC+75i7nBJb4J46KQigYauY3FRM2QyMoVb/YmiygAnmH7aj4mz1iAVUVh/wRyvMtA/Aq1bROtM2Pfu23bM/Z4hDJtC+53OMf/DVTILkSpN8AF6QRO0DcvjPFHDeKhcgQgtYh4q/tjLu38nmKe6yjzILk5TxGlCc/V9w5NO5fHs0oQIDAQAB',
+      serverVersion: '6452',
+      pluginsVersion: '1000.0.0.57d9ee1272a4',
+      baseUrl: 'https://whimet4.atlassian.net/wiki',
+      productType: 'confluence',
+      description: 'Atlassian Confluence at null',
+      eventType: 'uninstalled'
+    }
+     */
+    handleLifecycleEvent(req);
+    resp.send(`uninstalled`);
 });
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,11 @@
 import * as functions from "firebase-functions";
 import * as descriptorTemplate from "./atlassian-connect.json";
+import * as express from 'express';
+
+var Mixpanel = require('mixpanel');
+ 
+//Mixpanel project: Confluence Analytics
+var mixpanel = Mixpanel.init('0c62cea9ed2247f4824bf196f6817941');
 
 export const descriptor = functions.https.onRequest((req, resp) => {
   const url = req.url;
@@ -18,4 +24,62 @@ export const descriptor = functions.https.onRequest((req, resp) => {
 
 export const renderAttachment = functions.https.onRequest((request, response) => {
   response.send(`<ac:image> <ri:attachment ri:filename="zenuml-${request.query.uuid}.png" /> </ac:image>`);
+});
+
+function handleLifecycleEvent(req: express.Request) {
+  if(!req.body) {
+    return;
+  }
+
+  const clientSite = new URL(req.body.baseUrl).hostname;
+  mixpanel.track(req.body.eventType, {
+    distinct_id: clientSite,
+    clientSite,
+    key: req.body.key,
+    eventType: req.body.eventType,
+    serverVersion: req.body.serverVersion,
+    pluginVersion: req.body.pluginsVersion,
+    productType: req.body.productType,
+    description: req.body.description,
+    displayProductName: 'Async API'
+  });
+}
+
+export const installed = functions.https.onRequest((req, resp) => {
+  /* example request.body:
+   {
+    key: 'my-api',
+    clientKey: 'aa4a743a-201f-38c4-b7fd-d7f8f68ec685',
+    publicKey: 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC+75i7nBJb4J46KQigYauY3FRM2QyMoVb/YmiygAnmH7aj4mz1iAVUVh/wRyvMtA/Aq1bROtM2Pfu23bM/Z4hDJtC+53OMf/DVTILkSpN8AF6QRO0DcvjPFHDeKhcgQgtYh4q/tjLu38nmKe6yjzILk5TxGlCc/V9w5NO5fHs0oQIDAQAB',
+    sharedSecret: 'ATCOfzFTe2Wc3Rh3FRV2h9CUlZCBRpUhIF5LXKDgv1Ru17afsZ4_fPQRqx1PZd21mBMIM0RDjU-uZPxMegaUfwRR_A0678FE89',
+    serverVersion: '6452',
+    pluginsVersion: '1000.0.0.57d9ee1272a4',
+    baseUrl: 'https://whimet4.atlassian.net/wiki',
+    productType: 'confluence',
+    description: 'Atlassian Confluence at null',
+    eventType: 'installed'
+  }
+  */
+
+  handleLifecycleEvent(req);
+  resp.send(`installed`);
+});
+
+export const uninstalled = functions.https.onRequest((req, resp) => {
+  /** example request.body:
+  {
+    key: 'my-api',
+    clientKey: 'aa4a743a-201f-38c4-b7fd-d7f8f68ec685',
+    publicKey: 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC+75i7nBJb4J46KQigYauY3FRM2QyMoVb/YmiygAnmH7aj4mz1iAVUVh/wRyvMtA/Aq1bROtM2Pfu23bM/Z4hDJtC+53OMf/DVTILkSpN8AF6QRO0DcvjPFHDeKhcgQgtYh4q/tjLu38nmKe6yjzILk5TxGlCc/V9w5NO5fHs0oQIDAQAB',
+    serverVersion: '6452',
+    pluginsVersion: '1000.0.0.57d9ee1272a4',
+    baseUrl: 'https://whimet4.atlassian.net/wiki',
+    productType: 'confluence',
+    description: 'Atlassian Confluence at null',
+    eventType: 'uninstalled'
+  }
+   */
+
+  handleLifecycleEvent(req);
+  resp.send(`uninstalled`);
 });

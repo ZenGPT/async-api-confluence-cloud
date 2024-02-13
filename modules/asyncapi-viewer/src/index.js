142eba606e716a41bb2c2321773e94a2da67b429
Yanhui Li <whimet@gmail.com>
2024-02-13 23:04:22 +1100

    Fix asyncapi-viewer on export PDF support

---
+++ modules/asyncapi-viewer/src/index.js
@@ -0,0 +1,18 @@
+import * as htmlToImage from 'html-to-image';
+
+window.toPng = () => {
+  const root = window.rootElement || document.body;
+  return htmlToImage.toBlob(root, { bgcolor: 'white' });
+}
+
+window.addEventListener('message', async ({ source, data }) => {
+  if (data?.action === 'load') {
+    console.debug('loading data:\n', data?.data);
+    render(data?.data);
+  }
+  else if (data?.action === 'export') {
+    const data = await window.toPng();
+    source.postMessage({ action: 'export.result', data });
+    console.debug('asyncapi-viewer - PNG exported');
+  }
+})
